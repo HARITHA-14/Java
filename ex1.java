6a92118a2b2aed73b592e1133b877ae5f533af4d
Haritha sri Kathiresan <95612803+HARITHA-14@users.noreply.github.com>
2023-07-11 09:41:42 +0530

    Add files via upload

---
+++ ex1.java
@@ -0,0 +1,20 @@
+//java program to run greatest of two numbers
+import java.util.Scanner;
+public class ex1{
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int num1 =sc.nextInt();
+        int num2 =sc.nextInt();
+        if(num1>num2)
+        {
+            System.out.println("num1 is greater");
+        
+        }
+        else{
+            System.out.println("num2 is greater");
+        }
+        
+    }
+}
+ 
