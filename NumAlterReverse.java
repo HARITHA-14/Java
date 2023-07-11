888b117f250bd86a444c841fe1121ce094cf700b
Haritha sri Kathiresan <95612803+HARITHA-14@users.noreply.github.com>
2023-07-11 14:40:45 +0530

    Add files via upload

---
+++ NumAlterReverse.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class NumAlterReverse{
+    public static void main(String[]args){
+        int i=100;
+        for (i=100;i>=0;i-=3)
+        {
+            System.out.println(i);
+        }
+    }
+}
