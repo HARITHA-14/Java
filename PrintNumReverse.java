3db2399c149fd023fd1679618e37b495d70c49b2
Haritha sri Kathiresan <95612803+HARITHA-14@users.noreply.github.com>
2023-07-11 14:20:22 +0530

    Add files via upload

---
+++ PrintNumReverse.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class PrintNumReverse
+{
+    public static void main(String[]args){
+    int i =100;
+    while (i>=0){
+        System.out.println(i);
+        --i;
+    
+        
+    }
+    }
+}
