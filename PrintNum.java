9fd705a342638138df7a11787e25eb0d87986faa
Haritha sri Kathiresan <95612803+HARITHA-14@users.noreply.github.com>
2023-07-11 14:15:46 +0530

    Add files via upload

---
+++ PrintNum.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class PrintNum
+{
+    public static void main(String[]args){
+    int i=0;
+    while (i<=100){
+        System.out.println(i);
+        i++;
+    }
+    }
+}
